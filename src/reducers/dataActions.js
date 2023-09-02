daf4d1f065acb779baa75a6c33889ae010487af3
Maria <114153517+eyesmaybeyes@users.noreply.github.com>
2023-09-02 05:21:55 +0300

    added components

---
+++ src/reducers/dataActions.js
@@ -0,0 +1,50 @@
+import axios from 'axios';
+
+export const fetchDataRequest = () => ({
+    type: 'FETCH_DATA_REQUEST',
+});
+
+export const fetchDataSuccess = (data) => ({
+    type: 'FETCH_DATA_SUCCESS',
+    payload: data,
+});
+
+export const fetchDataFailure = (error) => ({
+    type: 'FETCH_DATA_FAILURE',
+    payload: error,
+});
+
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
+async function getImageForBreed(breed_id) {
+    const response = await axios.get(`https://api.thecatapi.com/v1/images/search?breed_id=${breed_id}`);
+    return response.data[0].url;
+}
+
+export const fetchData = () => {
+    return async (dispatch) => {
+        dispatch(fetchDataRequest());
+        try {
+            const response = await axios.get('https://api.thecatapi.com/v1/breeds/');
+
+            let allBreeds = response.data;
+            shuffleArray(allBreeds);
+            const randomBreeds = allBreeds.slice(0, 8);
+
+            const randomBreedsWithImage = await Promise.all(randomBreeds.map(async (breed) => {
+                const imageUrl = await getImageForBreed(breed.id);
+                return { ...breed, imageUrl };
+            }));
+
+            dispatch(fetchDataSuccess(randomBreedsWithImage));
+        } catch (error) {
+            dispatch(fetchDataFailure(error.message));
+        }
+    };
+};
---
+++ src/reducers/dataReducer.js
@@ -0,0 +1,25 @@
+const initialState = {
+    data: [],
+    loading: false,
+    error: null,
+};
+
+const dataReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'FETCH_DATA_REQUEST':
+            return { ...state, loading: true, error: null };
+        case 'FETCH_DATA_SUCCESS':
+            return { ...state, loading: false, data: action.payload };
+        case 'FETCH_DATA_FAILURE':
+            return { ...state, loading: false, error: action.payload };
+        case "DELETE_CARD":
+            return {
+                ...state,
+                data: action.payload,
+            };
+        default:
+            return state;
+    }
+};
+
+export default dataReducer;
